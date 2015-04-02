3d0639b2ba2dbcc983d45dfabd04fbb683446af3
AmandineF <amandinefouillet@gmail.com>
2015-04-01 20:59:42 -0400

    Contacts

--- IM/TP5/src/PanelContact.java
+++ IM/TP5/src/PanelContact.java
@@ -1,56 +1,133 @@
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.Border;
-
 import net.miginfocom.swing.MigLayout;
 
 
+@SuppressWarnings("serial")
 public class PanelContact extends JPanel {
 
 	/**
 	 * Create the panel.
 	 */
-	public PanelContact(String nom, String prenom, String numero, String image) {
+	public PanelContact(Contact contact) {
 		setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
+		final Color bg = getBackground();
 		setBackground(Color.WHITE);
-		//setLayout(new MigLayout("insets 2","9[15%]5[31%]3[9%]0[9%]0[9%]0[9%]0[9%]0[9%]","[50%][50%]"));
-		//setLayout(new MigLayout("insets 2","9[50]0[120]0[30]0[30]0[30]0[30]0[30]0[30]","[50%][50%]"));
 		setLayout(new MigLayout("insets 2","9[50]0[130]0[30]0[30]0[30]0[30]0[30]0[20]","[50%][50%]"));
 		
-		JLabel nomContact = new JLabel(nom + " " + prenom);
+		JLabel nomContact = new JLabel(contact.getNom() + " " + contact.getPrenom());
 		nomContact.setFont(new Font("Mockup", Font.PLAIN, 12));
 		this.add(nomContact, "cell 1 0, alignx left, aligny bottom");
 		
-		JLabel numeroContact = new JLabel(numero);
+		JLabel numeroContact = new JLabel(contact.getNumero());
 		numeroContact.setFont(new Font("Mockup", Font.PLAIN, 12));
 		this.add(numeroContact, "cell 1 1, alignx left, aligny top");
 		
-		JLabel imageContact = new JLabel(new ImageIcon(image));
+		JLabel imageContact = new JLabel(new ImageIcon(contact.getImage()));
 		this.add(imageContact, "cell 0 0 0 2, alignx left, aligny center");
 		
 		JLabel comContact = new JLabel(new ImageIcon("./Images/rondComContact.png"));
+		comContact.addMouseListener(new MouseAdapter() { 
+	          public void mousePressed(MouseEvent me) { 
+	        	  //removeAll();  
+	        	  //PanelBase m = new PanelBase(fen, "Communication");						
+	        	  //fen.setContentPane(m);
+	        	  //fen.validate();
+	          } 
+	          public void mouseEntered(MouseEvent e) {
+					setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR) );
+					setBackground(bg);
+				}	  		
+	          public void mouseExited(MouseEvent e) {
+					setCursor(Cursor.getDefaultCursor());
+					setBackground(Color.WHITE);
+				}
+	         }); 
 		this.add(comContact, "cell 2 0 0 2, alignx right, aligny center");
 		
 		JLabel phoContact = new JLabel(new ImageIcon("./Images/rondPhoContact.png"));
+		phoContact.addMouseListener(new MouseAdapter() { 
+	          public void mousePressed(MouseEvent me) { 
+	        	  //removeAll();  
+	        	  //PanelBase m = new PanelBase(fen, "Communication");						
+	        	  //fen.setContentPane(m);
+	        	  //fen.validate();
+	          } 
+	          public void mouseEntered(MouseEvent e) {
+					setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR) );
+					setBackground(bg);
+				}	  		
+	          public void mouseExited(MouseEvent e) {
+					setCursor(Cursor.getDefaultCursor());
+				}
+	         }); 
 		this.add(phoContact, "cell 3 0 0 2, alignx right, aligny center");
 		
 		JLabel calContact = new JLabel(new ImageIcon("./Images/rondCalContact.png"));
+		calContact.addMouseListener(new MouseAdapter() { 
+	          public void mousePressed(MouseEvent me) { 
+	        	  //removeAll();  
+	        	  //PanelBase m = new PanelBase(fen, "Communication");						
+	        	  //fen.setContentPane(m);
+	        	  //fen.validate();
+	          } 
+	          public void mouseEntered(MouseEvent e) {
+					setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR) );
+					setBackground(bg);
+				}	  		
+	          public void mouseExited(MouseEvent e) {
+					setCursor(Cursor.getDefaultCursor());
+					setBackground(Color.WHITE);
+				}
+	         }); 
 		this.add(calContact, "cell 4 0 0 2, alignx right, aligny center");
 		
 		JLabel depContact = new JLabel(new ImageIcon("./Images/rondDepContact.png"));
+		depContact.addMouseListener(new MouseAdapter() { 
+	          public void mousePressed(MouseEvent me) { 
+	        	  //removeAll();  
+	        	  //PanelBase m = new PanelBase(fen, "Communication");						
+	        	  //fen.setContentPane(m);
+	        	  //fen.validate();
+	          } 
+	          public void mouseEntered(MouseEvent e) {
+					setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR) );
+					setBackground(bg);
+				}	  		
+	          public void mouseExited(MouseEvent e) {
+					setCursor(Cursor.getDefaultCursor());
+					setBackground(Color.WHITE);
+				}
+	         }); 
 		this.add(depContact, "cell 5 0 0 2, alignx right, aligny center");
 		
 		JLabel carContact = new JLabel(new ImageIcon("./Images/rondCarContact.png"));
+		carContact.addMouseListener(new MouseAdapter() { 
+	          public void mousePressed(MouseEvent me) { 
+	        	  //removeAll();  
+	        	  //PanelBase m = new PanelBase(fen, "Communication");						
+	        	  //fen.setContentPane(m);
+	        	  //fen.validate();
+	          } 
+	          public void mouseEntered(MouseEvent e) {
+					setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR) );
+					setBackground(bg);
+				}	  		
+	          public void mouseExited(MouseEvent e) {
+					setCursor(Cursor.getDefaultCursor());
+					setBackground(Color.WHITE);
+				}
+	         }); 
 		this.add(carContact, "cell 6 0 0 2, alignx right, aligny center");
-		
-		//JLabel infoContact = new JLabel(new ImageIcon("./Images/rondComContact.png"));
-		//this.add(infoContact, "cell 7 0 0 2, alignx right, aligny center");
-		
+				
 	}
 
 }
--- IM/TP5/src/PanelContacts.java
+++ IM/TP5/src/PanelContacts.java
@@ -1,7 +1,10 @@
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.LinkedList;
 
 import javax.swing.BorderFactory;
@@ -16,6 +19,18 @@ import net.miginfocom.swing.MigLayout;
 @SuppressWarnings("serial")
 public class PanelContacts extends JPanel {
 
+	private class PanelLettre extends JPanel {
+		public PanelLettre(String l){
+			this.setLayout(new MigLayout("insets 0", "", ""));
+			this.setPreferredSize(new Dimension (348, 5));
+			JLabel lettre = new JLabel(l);
+			lettre.setBorder(BorderFactory.createEmptyBorder(0,5,0,0));
+			lettre.setFont(new Font("Mockup", Font.PLAIN, 10));
+			lettre.setForeground(Color.WHITE);
+			this.add(lettre, "cell 0 0, alignx left, aligny center");
+			this.setBackground(new Color(94,94,94));
+		}
+	}
 	/**
 	 * Create the panel.
 	 */
@@ -23,37 +38,42 @@ public class PanelContacts extends JPanel {
 		this.setLayout(new MigLayout("insets 0", "", ""));
 		JPanel sp = new JPanel();
 		MigLayout ml = new MigLayout("insets 0, gapy 0", "", "");
+		final Color bg = this.getBackground();
 		sp.setBackground(Color.WHITE);
 		sp.setLayout(ml);
 		LinkedList<Contact> listContact = UtilisateurManager.getInstance().getConnectedUser().getContact();
 		int nbContact = listContact.size();
 		
-		JPanel barreLettre = new JPanel();
-		barreLettre.setLayout(new MigLayout("insets 0", "", ""));
-		barreLettre.setPreferredSize(new Dimension (348, 5));
-		JLabel lettre = new JLabel("A");
-		lettre.setBorder(BorderFactory.createEmptyBorder(0,5,0,0));
-		lettre.setFont(new Font("Mockup", Font.PLAIN, 10));
-		lettre.setForeground(Color.WHITE);
-		barreLettre.add(lettre, "cell 0 0, alignx left, aligny center");
-		barreLettre.setBackground(new Color(68,68,68));
 		String old = "Z";
 		int j = 0;
 		for(int i = 0; i < nbContact; i++) {
-			/*
 			String firstLettre = listContact.get(i).getNom().substring(0, 1);
 			if(!(firstLettre.equals(old))){
-				lettre.setText(firstLettre);
-				sp.add(barreLettre, "cell 0 " + j + ",alignx left, aligny center");
+				PanelLettre pl = new PanelLettre(firstLettre);
+				sp.add(pl, "cell 0 " + j + ",alignx left, aligny center");
 				j++;
 				old = firstLettre;
 			}
-			*/
-			PanelContact pc = new PanelContact(listContact.get(i).getNom(), 
-					listContact.get(i).getPrenom(),
-					listContact.get(i).getNumero(),
-					listContact.get(i).getImage());
-			sp.add(pc, "cell 0 " + i + ",alignx center, aligny center");	
+			
+			final PanelContact pc = new PanelContact(listContact.get(i));
+			pc.addMouseListener(new MouseAdapter() { 
+		          public void mousePressed(MouseEvent me) { 
+		        	  //removeAll();  
+		        	  //PanelBase m = new PanelBase(fen, "Communication");						
+		        	  //fen.setContentPane(m);
+		        	  //fen.validate();
+		          } 
+		          public void mouseEntered(MouseEvent e) {
+						setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR) );
+						pc.setBackground(bg);
+					}	  		
+		          public void mouseExited(MouseEvent e) {
+						setCursor(Cursor.getDefaultCursor());
+						pc.setBackground(Color.WHITE);
+					}
+		         }); 
+			sp.add(pc, "cell 0 " + j + ",alignx center, aligny center");	
+			j++;
 		}
 		
 		JScrollPane contentScrollPane = new JScrollPane(sp);
